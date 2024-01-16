716d742d8a8290a6613ba71a2b3dadddf094aba6
Sauparna Palchowdhury <sauparna+git@icloud.com>
2024-01-16 08:03:13 -0700

    Correct prototypes.

--- sgb/gb_flip.h
+++ sgb/gb_flip.h
@@ -2,5 +2,5 @@
 extern long *gb_fptr;        /* the next A value to be used */
 extern long gb_flip_cycle(); /* compute 55 more pseudo-random numbers */
 #define mod_diff(x, y) (((x) - (y)) & 0x7fffffff) /* difference modulo 2^31 */
-extern void gb_init_rand();
-extern long gb_unif_rand();
+extern void gb_init_rand(long seed);
+extern long gb_unif_rand(long m);
