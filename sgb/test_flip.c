4c0a36c00eebe14689acaf8af8de8abd79557a25
Sauparna Palchowdhury <sauparna+git@icloud.com>
2024-01-30 19:45:38 -0700

    Bugfix: Rewrite decimal as hexadecimal value.

--- sgb/test_flip.c
+++ sgb/test_flip.c
@@ -2,15 +2,14 @@
 #include <stdio.h>
 
 int main() {
-  long j;
   gb_init_rand(-314159);
   if (gb_next_rand() != 119318998) {
     fprintf(stderr, "Failure on the first try!\n");
     return -1;
   }
-  for (int j = 1; j <= 133; j++)
+  for (long j = 1; j <= 133; j++)
     gb_next_rand();
-  if (gb_unif_rand(55555555) != 748103812) {
+  if (gb_unif_rand(0x55555555) != 748103812) {
     fprintf(stderr, "Failure on the second try!\n");
     return -2;
   }
